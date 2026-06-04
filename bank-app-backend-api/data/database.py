6efb2b4707072c5967dc536056cdeeedff30ed86
Giorch <69166473+Giorch@users.noreply.github.com>
2026-06-04 13:59:46 -0400

    Update frontend API URI

--- bank-app-backend-api/data/database.py
+++ bank-app-backend-api/data/database.py
@@ -6,7 +6,8 @@ load_dotenv()
  
 MONGO_URI = os.getenv("MONGO_URI")
 DB_NAME = os.getenv("DB_NAME", "BankApp")
- 
+
+print(DB_NAME)
 
 client = MongoClient(MONGO_URI)
 db = client[DB_NAME]
--- bank-app-frontend/src/api/DataService.js
+++ bank-app-frontend/src/api/DataService.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-const API_URL = "http://localhost:8000/api"
+const API_URL = "https://bank-app-project-cpxu.onrender.com/api"
 
 export const getAllCustomers = () =>
     axios.get(`${API_URL}/customers`);
