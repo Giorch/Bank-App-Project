c4cf871de38e525eca861f1113643384bdd55117
Giorch <69166473+Giorch@users.noreply.github.com>
2026-06-04 14:14:26 -0400

    Update database.py

--- bank-app-backend-api/data/database.py
+++ bank-app-backend-api/data/database.py
@@ -7,9 +7,6 @@ load_dotenv()
 MONGO_URI = os.getenv("MONGO_URI")
 DB_NAME = os.getenv("DB_NAME", "BankApp")
 
-print(MONGO_URI) #Render debugging
-print(DB_NAME)
-
 client = MongoClient(MONGO_URI)
 db = client[DB_NAME]
  
